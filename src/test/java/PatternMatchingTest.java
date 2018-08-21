ba1728cd7de1d9037e4003324add3421eca97dc9
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-21 13:59:30 +0200

    add test cases

--- src/test/java/PatternMatchingTest.java
+++ src/test/java/PatternMatchingTest.java
@@ -2,6 +2,7 @@ import io.functional.PatternMatching;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PatternMatchingTest {
 
@@ -12,5 +13,48 @@ public class PatternMatchingTest {
                 .execute();
 
         assertThat(length).isEqualTo(3);
+    }
+
+    @Test
+    public void returns_first_registered_type_rule() {
+        String result = PatternMatching.when("abc")
+                .is(String.class).thenReturn(s -> s + "1")
+                .is(String.class).thenReturn(s -> s + "2")
+                .execute();
+
+        assertThat(result).isEqualTo("abc1");
+    }
+
+    @Test
+    public void executes_operations_in_lazy_mode() {
+        PatternMatching.ExecuteOperation<String, Object> operation = PatternMatching.when("abc")
+                .is(String.class).thenReturn(s -> {
+                    throw new RuntimeException("lazy one!");
+                });
+
+        assertThatThrownBy(operation::execute).hasMessage("lazy one!");
+    }
+
+    class A {}
+    class B extends A {}
+
+    @Test
+    public void match_parent_class_firstly_as_more_generic() {
+        String result = PatternMatching.when(new B())
+                .is(A.class).thenReturn(c -> "A")
+                .is(B.class).thenReturn(c -> "B")
+                .execute();
+
+        assertThat(result).isEqualTo("A");
+    }
+
+    @Test
+    public void match_parent_class_firstly_as_more_generic_even_if_it_was_defined_after_other_matches() {
+        String result = PatternMatching.when(new B())
+                .is(B.class).thenReturn(c -> "B")
+                .is(A.class).thenReturn(c -> "A")
+                .execute();
+
+        assertThat(result).isEqualTo("A");
     }
 }
