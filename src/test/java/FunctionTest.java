91cc77341d9d6bf5a72a7e74e686191f07712ca3
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-22 21:00:44 +0200

    - add further methods

--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -5,10 +5,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FunctionTest {
 
-    @Test
-    public void composes_functions() {
+    private final Function<Integer, Integer> square = x -> x * x;
+    private final Function<Integer, Integer> times2 = x -> x * 2;
 
-        Integer result = Function.<Integer, Integer, Integer>compose()
+    @Test
+    public void construct_and_run_simple_function() {
+        Function<Integer, Double> times3AndConvertToDouble = x -> (double) (x * 3);
+        Double result = times3AndConvertToDouble.apply(2);
+
+        assertThat(result).isEqualTo(6.0);
+    }
+
+    @Test
+    public void combine_function_with_another_ones() {
+        Function<Integer, Integer> times2AndThenSquare = square.compose(times2);
+
+        Integer result = times2AndThenSquare.apply(2);
+
+        assertThat(result).isEqualTo(16);
+    }
+
+    @Test
+    public void apply_another_func_after_defined_one() {
+        Function<Integer, Integer> squareAndThenTimes2 = square.andThen(times2);
+
+        Integer result = squareAndThenTimes2.apply(2);
+
+        assertThat(result).isEqualTo(8);
+    }
+
+    @Test
+    public void high_order_compose() {
+
+        Integer result = Function.<Integer, Integer, Integer>higherCompose()
                 .apply(x -> x * x)
                 .apply(x -> x * 3)
                 .apply(2);
@@ -17,9 +46,9 @@ public class FunctionTest {
     }
 
     @Test
-    public void chain_functions() {
+    public void higher_and_then() {
 
-        Integer result = Function.<Integer, Integer, Integer>andThen()
+        Integer result = Function.<Integer, Integer, Integer>higherAndThen()
                 .apply(x -> x * x)
                 .apply(x -> x * 3)
                 .apply(2);
@@ -30,7 +59,7 @@ public class FunctionTest {
     @Test
     public void composes_functions_of_different_types() {
 
-        Integer result = Function.<Long, Double, Integer>compose()
+        Integer result = Function.<Long, Double, Integer>higherCompose()
                 .apply(a -> (int) (a * 3))
                 .apply(a -> a + 2.0)
                 .apply(1L);
@@ -41,7 +70,7 @@ public class FunctionTest {
     @Test
     public void chain_functions_of_different_types() {
 
-        Integer result = Function.<Long, Double, Integer>andThen()
+        Integer result = Function.<Long, Double, Integer>higherAndThen()
                 .apply(a -> a + 2.0)
                 .apply(a -> (int) (a * 3))
                 .apply(1L);
