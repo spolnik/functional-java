fa5c06afde882b41070c96313c9dde04f77fb302
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-22 09:51:23 +0200

    - no need to name input funcs ...

--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -7,40 +7,44 @@ public class FunctionTest {
 
     @Test
     public void composes_functions() {
-        Function<Integer, Integer> triple = x -> x * 3;
-        Function<Integer, Integer> square = x -> x * x;
 
-        Integer result = Function.<Integer, Integer, Integer>compose().apply(square).apply(triple).apply(2);
+        Integer result = Function.<Integer, Integer, Integer>compose()
+                .apply(x -> x * x)
+                .apply(x -> x * 3)
+                .apply(2);
 
         assertThat(result).isEqualTo(36);
     }
 
     @Test
     public void chain_functions() {
-        Function<Integer, Integer> triple = x -> x * 3;
-        Function<Integer, Integer> square = x -> x * x;
 
-        Integer result = Function.<Integer, Integer, Integer>andThen().apply(square).apply(triple).apply(2);
+        Integer result = Function.<Integer, Integer, Integer>andThen()
+                .apply(x -> x * x)
+                .apply(x -> x * 3)
+                .apply(2);
 
         assertThat(result).isEqualTo(12);
     }
 
     @Test
     public void composes_functions_of_different_types() {
-        Function<Double, Integer> f = a -> (int) (a * 3);
-        Function<Long, Double> g = a -> a + 2.0;
 
-        Integer result = Function.<Long, Double, Integer>compose().apply(f).apply(g).apply(1L);
+        Integer result = Function.<Long, Double, Integer>compose()
+                .apply(a -> (int) (a * 3))
+                .apply(a -> a + 2.0)
+                .apply(1L);
 
         assertThat(result).isEqualTo(9);
     }
 
     @Test
     public void chain_functions_of_different_types() {
-        Function<Double, Integer> f = a -> (int) (a * 3);
-        Function<Long, Double> g = a -> a + 2.0;
 
-        Integer result = Function.<Long, Double, Integer>andThen().apply(g).apply(f).apply(1L);
+        Integer result = Function.<Long, Double, Integer>andThen()
+                .apply(a -> a + 2.0)
+                .apply(a -> (int) (a * 3))
+                .apply(1L);
 
         assertThat(result).isEqualTo(9);
     }
